254ffcac20fd3a35e48c47ab6fca826e78e88643
Jon St John <jon@cockroachlabs.com>
2021-12-10 09:52:18 -0700

    Add knex javascript example

--- typescript/node-postgres/app.ts
+++ typescript/node-postgres/app.ts
@@ -6,17 +6,17 @@ import { Pool, Client } from 'pg';
 //
 
 // Must be changed
-const username = 'app' // change this
-const password = 'password' // change this
-const certdir = '/Users/jonstjohn' // change this to your home directory or the directory where your certs are
-const cluster = 'sweet-donkey-123' // change this
+const username = process.env.CRDB_USERNAME || 'app' // change this
+const password = process.env.CRDB_PASSWORD || 'password' // change this
+const certdir = process.env.CRDB_CERTDIR || '/Users/jonstjohn' // change this to your home directory or the directory where your certs are
+const cluster = process.env.CRDB_CLUSTER || 'sweet-donkey-123' // change this
 
 // Might need to be changed
-const database = 'defaultdb' // may need to change this
-const host = 'free-tier.gcp-us-central1.cockroachlabs.cloud' // may need to change this
+const database = process.env.CRDB_DATABASE || 'defaultdb' // may need to change this
+const host = process.env.CRDB_HOST || 'free-tier.gcp-us-central1.cockroachlabs.cloud' // may need to change this
 
 // Probably don't need to change this
-const certname = 'root.crt'
+const certname = process.env.CRDB_CERTNAME || 'root.crt'
 
 //
 // BUILD CONNECTION STRING AND CONNECT
