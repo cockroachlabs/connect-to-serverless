1c42cfc1afb696b1813c9147a5978744c5945792
Jon St John <jon@cockroachlabs.com>
2021-12-10 08:49:49 -0700

    Add node-postgres typescript example

---
+++ typescript/node-postgres/app.ts
@@ -0,0 +1,36 @@
+// Use the node-postgres library
+import { Pool, Client } from 'pg';
+
+//
+// CONFIGURATION
+//
+
+// Must be changed
+const username = 'app' // change this
+const password = 'password' // change this
+const certdir = '/Users/jonstjohn' // change this to your home directory or the directory where your certs are
+const cluster = 'sweet-donkey-123' // change this
+
+// Might need to be changed
+const database = 'defaultdb' // may need to change this
+const host = 'free-tier.gcp-us-central1.cockroachlabs.cloud' // may need to change this
+
+// Probably don't need to change this
+const certname = 'root.crt'
+
+//
+// BUILD CONNECTION STRING AND CONNECT
+//
+// Create the connection string
+const connectionString = 'postgresql://' + username + ':' + password + '@' + host + ':26257/' + database + '?sslmode=verify-full&sslrootcert=' + certdir + '/.postgresql/' + certname + '&options=--cluster%3D' + cluster
+const pool = new Pool({
+  connectionString,
+})
+
+//
+// EXECUTE QUERY
+//
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
